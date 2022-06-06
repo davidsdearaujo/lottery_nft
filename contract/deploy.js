ecd2cbf309d9ac2f994f450981c9ae54d210e86d
LeBaleiro <lehbaleiro@gmail.com>
2022-06-05 22:37:40 -0300

    restructure repository

---
+++ contract/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'real genius snow feed good drop danger define scatter panther crush hard',
+  'https://rinkeby.infura.io/v3/9418e02a3f6d470f8e723aa8f73594f8'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
